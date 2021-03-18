80d472c37f2ce2754c083637e88049e2f09a13a0
root <root@DESKTOP-O8D4QVB.localdomain>
2021-03-18 16:37:38 +0000

    main & flags eucledian

--- src/ballAlg.c
+++ src/ballAlg.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gen_points.h"
+#include <omp.h>
+#include <math.h>
+
+
+double** pts;
+int n_dims;
+long n_points;
+int root;
+
+
+double distance(double *pt1, double *pt2)
+{
+    double dist = 0.0;
+
+    for(int d = 0; d < n_dims; d++)
+        dist += (pt1[d] - pt2[d]) * (pt1[d] - pt2[d]);
+    return sqrt(dist);
+}
+
+
+int build_tree(){
+    return 0;
+}
+
+
 
 int main(int argc, char** argv) {
-
+    double exec_time;
+    exec_time = -omp_get_wtime();
+    pts = get_points(argc, argv, &n_dims, &n_points);
+    root = build_tree();
+    exec_time += omp_get_wtime();
+    fprintf(stderr, "%.1lf\n", exec_time);
+    //dump_tree(root); 
 }
+
