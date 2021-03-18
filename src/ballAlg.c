89262ac5b3343abb4b9ecebb2b2fc7754b5b57d4
root <root@DESKTOP-O8D4QVB.localdomain>
2021-03-18 17:08:23 +0000

    print dimensions of points

--- src/ballAlg.c
+++ src/ballAlg.c
@@ -20,8 +20,34 @@ double distance(double *pt1, double *pt2)
     return sqrt(dist);
 }
 
+long getmaxdistanceforindex_x(long index){
+    double max = 0.0;
+    long indexmax = index;
+    for(int p = 0; p < n_points; p++){
+        if(distance(pts[index], pts[p]) > max){
+            max = distance(pts[index], pts[p]);
+            indexmax = p;
+        }        
+    }
+    return indexmax;
+}
+
+void print_point(long index){
+    double* point = pts[index];
+    printf("the point of index %ld is: ", index);
+    for(int i = 0; i < n_dims; i++){
+           printf(" %.2f  ", point[i]);
+    }
+    printf("\n");
+}
+
 
 int build_tree(){
+
+    long farawaypoint= getmaxdistanceforindex_x(0);
+
+    print_point(farawaypoint);
+    
     return 0;
 }
 
