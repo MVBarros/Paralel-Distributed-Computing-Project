13845871aefe9eb7d2cc1e41882c008eff556191
root <root@DESKTOP-O8D4QVB.localdomain>
2021-03-18 18:11:37 +0000

    othogonal method

--- src/ballAlg.c
+++ src/ballAlg.c
@@ -32,7 +32,7 @@ long getmaxdistanceforindex_x(long index){
     return indexmax;
 }
 
-void print_point(long index){
+void print_index(long index){
     double* point = pts[index];
     printf("the point of index %ld is: ", index);
     for(int i = 0; i < n_dims; i++){
@@ -41,13 +41,95 @@ void print_point(long index){
     printf("\n");
 }
 
+void print_point(double* point){
+    for(int i = 0; i < n_dims; i++){
+           printf(" %.2f  ", point[i]);
+    }
+    printf("\n");
+}
+
+double* mult_point(double* a, double* b){
+
+    double* c = (double*) malloc(sizeof(double)*n_dims);
+    for(int i = 0; i < n_dims; i++){
+        c[i] = a[i] * b[i];
+    }
+    return c;
+}
+
+double* mult_scalar(double* a, double b){
+
+    double* c = (double*) malloc(sizeof(double)*n_dims);
+    for(int i = 0; i < n_dims; i++){
+        c[i] = a[i] * b;
+    }
+    return c;
+}
+
+double dotproduct_points(double* a, double* b){
+    double c = 0;
+    for(int i = 0; i < n_dims; i++){
+        c += a[i] * b[i];
+    }
+    return c;
+}
+
+
+
+double* sum_points(double* a, double* b){
+    double* c = (double*) malloc(sizeof(double)*n_dims);
+    for(int i = 0; i < n_dims; i++){
+        c[i] = a[i] + b[i];
+    }
+    return c;
+}
+
+double* sub_points(double* a, double* b){
+    double* c = (double*) malloc(sizeof(double)*n_dims);
+    for(int i = 0; i < n_dims; i++){
+        c[i] = a[i] - b[i];
+    }
+    return c;
+}
+
+
+/*
+po =
+(p − a) · (b − a)
+(b − a) · (b − a)
+(b − a) + a
+*/
+
+double* orthogonal(long a, long b, long p){
+    double* basub = sub_points(pts[b],pts[a]);
+    double* pasub = sub_points(pts[p],pts[a]);
+    double c = dotproduct_points(pasub,basub);
+    double d = dotproduct_points(basub,basub);
+    double e = c/d;
+    double* f = mult_scalar(basub,e);
+    return sum_points(f, pts[a]);
+
+}
+
+
+
 
 int build_tree(){
 
-    long farawaypoint= getmaxdistanceforindex_x(0);
+    long a= getmaxdistanceforindex_x(0);
 
-    print_point(farawaypoint);
+    print_index(a);
     
+    long b = getmaxdistanceforindex_x(a);
+
+    print_index(b);
+
+    for(long i = 0; i < n_points; i++){
+        print_point(orthogonal(b,a,i));
+    }
+
+
+
     return 0;
 }
 
