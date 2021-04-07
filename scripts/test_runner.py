57bc603ada4dfcaf67ba9d6878bab40feed55f17
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-04-07 17:33:31 +0100

    Fix test runner

--- scripts/test_runner.py
+++ scripts/test_runner.py
@@ -28,13 +28,13 @@ query_args = ['3 1',
               '8 6 4 2'
               ]
 
-query_outputs = ['2.777747 5.539700',
-                '7.830992 7.984400',
-                '0.809638 0.609105 2.107746 2.097437 5.055056 7.446720 5.392867 8.758850 9.246286 3.082144 0.496353 2.529218 0.473570 3.954821 6.931734 5.535411 8.118067 9.333055 0.469908 2.162746',
-                '3.979046 5.032039 6.011886',
-                '1.996719 4.012344 5.988101 8.081113',
-                '1.003042 4.986528 9.010856',
-                '7.939939 5.934679 3.951869 1.930474'
+query_outputs = [b'2.777747 5.539700',
+                b'7.830992 7.984400',
+                b'0.809638 0.609105 2.107746 2.097437 5.055056 7.446720 5.392867 8.758850 9.246286 3.082144 0.496353 2.529218 0.473570 3.954821 6.931734 5.535411 8.118067 9.333055 0.469908 2.162746',
+                b'3.979046 5.032039 6.011886',
+                b'1.996719 4.012344 5.988101 8.081113',
+                b'1.003042 4.986528 9.010856',
+                b'7.939939 5.934679 3.951869 1.930474'
                 ]
 
 tree_files = ['./trees/ex-' + arg.replace(' ', '-') + '.tree' for arg in alg_args] 
@@ -51,7 +51,7 @@ for args, tree_file in zip(alg_args, tree_files):
         subprocess.run([executable, *args], stdout=tree_fd, stderr=subprocess.DEVNULL)
 
 for args, expected_out in zip(query_args, query_outputs):
-    result = subprocess.run(['./ballQuery', *args], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    result = subprocess.run(['../src/ballQuery', *args], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     if result.stdout.strip() == expected_out.strip():
         print('.', end='', flush=True)
     else:
