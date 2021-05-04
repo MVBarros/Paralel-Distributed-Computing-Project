e67421038613bee5636cb8907b850c6bd25598da
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-05-04 19:22:41 +0100

    remove omp code

--- make_omp_submission.sh
+++
@@ -1,8 +0,0 @@
-rm g29omp.zip
-mkdir g29omp
-cp src/*.c g29omp
-cp src/*.h g29omp
-cp src/Makefile g29omp
-cp docs/g29report.pdf g29omp
-zip g29omp.zip g29omp/*
-rm g29omp -r
--- src/merge_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef MERGE_SORT_H
-#define MERGE_SORT_H
-
-int compare_node(const void* pt1, const void* pt2);
-
-void merge_sort(double** a, double** b, long n, int max_depth);
-
-#endif
