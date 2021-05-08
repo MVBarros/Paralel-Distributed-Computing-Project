80a0cd2d4f689c25215ec5d02ced2f71bd147f8e
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-05-08 17:18:11 +0100

    call get_points in main and finalize in the end

--- src/ballAlg-mpi.c
+++ src/ballAlg-mpi.c
@@ -15,7 +15,8 @@ double **ortho_array; // list of ortogonal projections of the points in pts
 double **ortho_array_srt; //list of ortogonal projections of the point in pts to be sorted.
 double **pts_aux; // list of points of the next iteration of the algorithm
 
-long n_points; //number of points in the dataset
+long n_points_local; //number of points in the dataset present at this process
+long n_points_global; //number of points in the dataset present at all processes
 
 double  *basub; // point containing b-a for the orthogonal projections
 double *ortho_tmp; // temporary pointer used for calculation the orthogonal projection
@@ -39,7 +40,7 @@ Returns the point in pts furthest away from point p
 double* get_furthest_away_point(double* p) {
     double max_distance = 0.0;
     double* furthest_point = p;
-    for(long i = 0; i < n_points; i++){
+    for(long i = 0; i < n_points_local; i++){
         double curr_distance = distance(p, pts[i]);
         if(curr_distance > max_distance){
             max_distance = curr_distance;
@@ -81,16 +82,16 @@ Returns the median projection of the dataset
 by sorting the projections based on their x coordinate
 */
 double* get_center() {
-    memcpy(ortho_array_srt, ortho_array, sizeof(double*) * n_points);
-    qsort(ortho_array_srt, n_points, sizeof(double*), compare_node);
+    memcpy(ortho_array_srt, ortho_array, sizeof(double*) * n_points_local);
+    qsort(ortho_array_srt, n_points_local, sizeof(double*), compare_node);
 
-    if(n_points % 2) { // is odd
-        long middle = (n_points - 1) / 2;
+    if(n_points_local % 2) { // is odd
+        long middle = (n_points_local - 1) / 2;
         copy_point(ortho_array_srt[middle], node_centers[node_id]);
     }
     else { // is even
-        long first_middle = (n_points / 2) - 1;
-        long second_middle = (n_points / 2);
+        long first_middle = (n_points_local / 2) - 1;
+        long second_middle = (n_points_local / 2);
 
         middle_point(ortho_array_srt[first_middle], ortho_array_srt[second_middle], node_centers[node_id]);
     }
@@ -102,7 +103,7 @@ Computes the orthogonal projections of points in pts onto line defined by b-a
 */
 void calc_orthogonal_projections(double* a, double* b) {
     sub_points(b, a, basub);
-    for(long i = 0; i < n_points; i++){
+    for(long i = 0; i < n_points_local; i++){
         orthogonal_projection(basub, a, pts[i], ortho_array[i], ortho_tmp);
     }
 }
@@ -114,7 +115,7 @@ of its orthogonal projection with the x coordinate of the center point
 void fill_partitions(double** left, double** right, double* center) {
     long l = 0;
     long r = 0;
-    for(long i = 0; i < n_points; i++) {
+    for(long i = 0; i < n_points_local; i++) {
         if(ortho_array[i][0] < center[0]) {
             left[l] = pts[i];
             l++;
@@ -127,7 +128,7 @@ void fill_partitions(double** left, double** right, double* center) {
 }
 
 void build_tree() {
-    if(n_points == 1) {
+    if(n_points_local == 1) {
         make_node(node_id, pts[0], 0, &node_list[node_id]);
         return;
     }
@@ -146,13 +147,13 @@ void build_tree() {
     double **pts_aux_left = pts;
     double **ortho_array_left = ortho_array;
     double **ortho_array_srt_left = ortho_array_srt;
-    long n_points_left = LEFT_PARTITION_SIZE(n_points);
+    long n_points_local_left = LEFT_PARTITION_SIZE(n_points_local);
 
-    double **right = pts_aux + n_points_left;
-    double **pts_aux_right = pts + n_points_left;
-    double **ortho_array_right = ortho_array + n_points_left;
-    double **ortho_array_srt_right = ortho_array_srt + n_points_left;
-    long n_points_right = RIGHT_PARTITION_SIZE(n_points);
+    double **right = pts_aux + n_points_local_left;
+    double **pts_aux_right = pts + n_points_local_left;
+    double **ortho_array_right = ortho_array + n_points_local_left;
+    double **ortho_array_srt_right = ortho_array_srt + n_points_local_left;
+    long n_points_local_right = RIGHT_PARTITION_SIZE(n_points_local);
 
     long node_id_left = ++node_counter;
     long node_id_right = ++node_counter;
@@ -163,7 +164,7 @@ void build_tree() {
     pts_aux = pts_aux_left;
     ortho_array = ortho_array_left;
     ortho_array_srt = ortho_array_srt_left;
-    n_points = n_points_left;
+    n_points_local = n_points_local_left;
     node_id = node_id_left;
     build_tree();
 
@@ -171,7 +172,7 @@ void build_tree() {
     pts_aux = pts_aux_right;
     ortho_array = ortho_array_right;
     ortho_array_srt = ortho_array_srt_right;
-    n_points = n_points_right;
+    n_points_local = n_points_local_right;
     node_id = node_id_right;
     build_tree();
 
@@ -180,12 +181,12 @@ void build_tree() {
 }
 
 void alloc_memory() {
-    n_nodes = (n_points * 2) - 1;
-    ortho_array = create_array_pts(n_dims, n_points);
-    ortho_array_srt = (double**) malloc(sizeof(double*) * n_points);
+    n_nodes = (n_points_local * 2) - 1;
+    ortho_array = create_array_pts(n_dims, n_points_local);
+    ortho_array_srt = (double**) malloc(sizeof(double*) * n_points_local);
     basub = (double*) malloc(sizeof(double) * n_dims);
     ortho_tmp = (double*) malloc(sizeof(double) * n_dims);
-    pts_aux = (double**) malloc(sizeof(double*) * n_points);
+    pts_aux = (double**) malloc(sizeof(double*) * n_points_local);
     node_list = (node_ptr) malloc(sizeof(node_t) * n_nodes);
     node_centers = create_array_pts(n_dims, n_nodes);
 }
@@ -198,14 +199,18 @@ int main(int argc, char** argv) {
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &n_procs);
 
+    pts = get_points(argc, argv, &n_dims, &n_points_global);
+
     /*
-    pts = get_points(argc, argv, &n_dims, &n_points);
     alloc_memory();
     build_tree();
     */
 
     exec_time += omp_get_wtime();
+    /*
     fprintf(stderr, "%.1lf\n", exec_time);
     printf("%d %ld\n", n_dims, n_nodes);
     dump_tree();
+    */
+    MPI_Finalize();
 }
