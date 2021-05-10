d647ea637a31fecb60237aa5c24534dc7bb65286
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-05-10 08:44:36 +0100

    update run_parallel_tests for mpi

--- scripts/run_parallel_test.sh
+++ scripts/run_parallel_test.sh
@@ -1,8 +1,12 @@
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $0 <mpirun|srun>"
+	exit 1
+fi
+
 for i in {1..5}
 do
-	for n in 1 2 4 8
+	for n in 1 2 4 8 16 32 64
 	do
-		export OMP_NUM_THREADS="${n}"
-		python3 test_runner.py ../src/ballAlg-omp
+		python3 test_runner_mpi.py $1 ../src/ballAlg-mpi ${n}
 	done
 done
