db03341cd2e596c94a9aebff1705224a30c48457
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-05-13 10:37:04 +0100

    fix size of buffers

--- src/ballAlg-mpi.c
+++ src/ballAlg-mpi.c
@@ -401,7 +401,8 @@ void mpi_dump_tree() {
 }
 
 void alloc_memory() {
-    current_buffer_size = (long) (ceil((double) (n_points_global) / (double) (n_procs)));
+    long min_split = pow(2, floor(log2(n_procs)));
+    current_buffer_size = (long) (ceil((double) (n_points_global) / (double) (min_split)));
 
     n_points_local = BLOCK_SIZE(rank, n_procs, n_points_global);
     n_nodes = (n_points_global * 2) - 1;
