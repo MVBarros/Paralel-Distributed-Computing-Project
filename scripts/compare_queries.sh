53469b542bf93163b3f018a859b915dec230c504
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-05-14 21:40:54 +0100

    add compare queries script

---
+++ scripts/compare_queries.sh
@@ -0,0 +1,9 @@
+queries=("20-1000000-0.query" "3-20000000-0.query" "4-10000000-0.query" "4-20000000-0.query" "3-5000000-0.query")
+
+for n in 1 2 4 8 16 32 64
+do
+    for query in "${queries[@]}"
+    do
+        diff --strip-trailing-cr queries/expected/${query} queries/obtained/${n}/${query}
+    done
+done
