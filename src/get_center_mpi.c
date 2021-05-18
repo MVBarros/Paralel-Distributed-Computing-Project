e164f5e758015505511f4c9d75ef80ab557b0027
ist187691 <miguel.v.barros@tecnico.ulisboa.pt>
2021-05-18 16:53:23 +0100

    Cleanup. Add Comments. Optimization to receive sort
    cleanup

--- src/get_center_mpi.c
+++ src/get_center_mpi.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <float.h>
 #include <mpi.h>
 #include "point_operations.h"
 #include "gen_points.h"
@@ -178,16 +179,20 @@ void psrs_sort_local_projections() {
     qsort(ortho_array_srt, n_points_local, sizeof(double*), compare_point);
 }
 
-void psrs_count_values_to_send(int* send_counts,int* send_displays, double* pivots){
-
+/*
+Places in send_counts how much data elements will be sent to each process and in send_displays
+the displacement of said data.
+Each process i receives the points before pivot at index i.
+*/
+void psrs_get_send_info(double* pivots, int *send_counts, int *send_displays){
+    memset(send_counts, 0, sizeof(int) * n_procs);
     long j = 0;
     long k = 0;
-    memset(send_counts, 0, sizeof(int) * n_procs);
-    for(long i = 0; i < n_points_local && j < n_procs-1 ;i++){
+    for(long i = 0; i < n_points_local && j != n_procs - 1 ;i++){
         if (ortho_array_srt[i][0] > pivots[j]){
-            send_counts[j]= i-k;
+            send_counts[j] = i - k;
             j++;
-            k=i;
+            k = i;
         }
     }
     send_counts[j] = n_points_local - k;
@@ -195,47 +200,58 @@ void psrs_count_values_to_send(int* send_counts,int* send_displays, double* pivo
     int count = 0;
     for(int i = 0; i < n_procs; i++){
         send_counts[i] *= n_dims;
-        send_displays[i]=count;
+        send_displays[i] = count;
         count += send_counts[i];
     }
 }
 
-
-long mpi_psrs_count_values_to_receive(int* send_counts, int* receive_counts, int* receive_displays){
+/*
+Places in receive_counts how much data elements the current process will receive from each process and in receive_displays
+the displacement of said data.
+Returns the total ammount of points received.
+*/
+long psrs_get_receive_info(int *send_counts, int *receive_counts, int *receive_displays){
+    /* Broadcast all-to-all each process sends to each process what is should sent him */
     MPI_Alltoall(
-        send_counts,
-        1,
-        MPI_INT,
-        receive_counts,
-        1,
-        MPI_INT,
-        communicator
+                send_counts,            /* send how much I will send each process */
+                1,                      /* send one value to each process */
+                MPI_INT,                /* value of type int */
+                receive_counts,         /* store how much i should receive in receive_counts */
+                1,                      /* receive one value from each process */
+                MPI_INT,                /* value of type int */
+                communicator            /* sending and receiving to all processes in the current team */
     );
 
     int count = 0;
     for(int i = 0; i < n_procs; i++){
-        receive_displays[i]=count;
+        receive_displays[i] = count;
         count += receive_counts[i];
     }
 
-    return (receive_displays[n_procs - 1] + receive_counts[n_procs - 1])/n_dims;
+    return (receive_displays[n_procs - 1] + receive_counts[n_procs - 1]) / n_dims;
 }
 
-double** mpi_psrs_points_exchange(int* send_counts, int* send_displays, int* receive_counts, int* receive_displays, long n_points_receive){
-    double** send_buffer = create_array_pts(n_dims, n_points_local);
-    copy_point_list(ortho_array_srt,send_buffer,n_points_local);
-    double** receive_buffer = create_array_pts(n_dims, n_points_receive);
-    
+/*
+Transfers to each process his partition of the points.
+Process i receives all the points smaller than pivot i from all other processes.
+Returns a pointer to the received data.
+*/
+double** mpi_psrs_exchange_projections(int *send_counts, int *send_displays, int *receive_counts, int *receive_displays, long n_points_receive) {
+    double **send_buffer = create_array_pts(n_dims, n_points_local);
+    double **receive_buffer = create_array_pts(n_dims, n_points_receive);
+
+    copy_point_list(ortho_array_srt, send_buffer, n_points_local);
+
     MPI_Alltoallv(
-        *send_buffer,
-        send_counts,
-        send_displays,
-        MPI_DOUBLE,
-        receive_buffer,
-        receive_counts,
-        receive_displays,
-        MPI_DOUBLE,
-        communicator
+                *send_buffer,       /* starting address of the data to send*/
+                send_counts,        /* array stating for each process i the number of data elements the current process will send them */
+                send_displays,      /* array stating for each process i the displacement of the data sent to them in send_buffer */
+                MPI_DOUBLE,         /* sending data elements of type double */
+                *receive_buffer,    /* starting address of where incoming data will be received */
+                receive_counts,     /* array stating for each process i the number of data elements the current process will receive from them */
+                receive_displays,   /* array stating for each process i the displacement of the data received from them in recv_buffer */
+                MPI_DOUBLE,         /* receiving data elements of type double */
+                communicator        /* sending and receiving to all processes in the current team */
     );
 
     free(*send_buffer);
@@ -244,18 +260,64 @@ double** mpi_psrs_points_exchange(int* send_counts, int* send_displays, int* rec
     return receive_buffer;
 }
 
-void mpi_psrs_gather_npoints_receive(long* receive_array ,long n_points_receive){
-    MPI_Allgather(
-        &n_points_receive,
-        1,
-        MPI_LONG,
-        receive_array,
-        1,
-        MPI_LONG,
-        communicator
+/*
+Normalizes receive_counts and receive_displays from storing the number of data elements
+to storing the number of points received.
+*/
+void psrs_normalize_receive_info(int *receive_counts, int *receive_displays) {
+    for(int i = 0; i < n_procs; i++) {
+        receive_counts[i] = receive_counts[i] / n_dims;
+
+    }
+    for(int i = 0; i < n_procs; i++) {
+        receive_displays[i] = receive_displays[i] / n_dims;
+    }
+}
+/*
+Place into receive_processes_n_points the number of points received at each process (n_points_receive)
+Asynchronous operation. The algorithm does useful work while it's receiving (namely merging the received points)
+*/
+void mpi_async_psrs_gather_n_points_receive(long *receive_processes_n_points ,long n_points_receive, MPI_Request *request){
+    MPI_Iallgather(
+                &n_points_receive,          /* address of sent data */
+                1,                          /* number of data elements to send */
+                MPI_LONG,                   /* sent elements of type long */
+                receive_processes_n_points, /* adress where received data is stored */
+                1,                          /* number of data elements received from each process */
+                MPI_LONG,                   /* receive elements of type long */
+                communicator,               /* communicate with entire team */
+                request                     /* place in request the asynchronous request */
     );
 }
 
+/*
+Sorts point list points of size n that is split in p sorted partitions whose size and displacement
+is given by counts and displays and writes the result in out
+*/
+void psrs_merge_sorted_point_list_partitions(double **points, int *receive_counts, int *receive_displays, long n, double **out) {
+    psrs_normalize_receive_info(receive_counts, receive_displays);
+    int receive_indexes[n_procs];
+    memset(receive_indexes, 0, n_procs * sizeof(int));
+
+    for(long i = 0; i < n; i++) {
+        double min = DBL_MAX;
+        double *point = NULL;
+        int k = 0;
+        for(int j = 0; j < n_procs; j++) {
+            if (receive_counts[j] == receive_indexes[j]) {
+                continue;
+            }
+            long l = receive_displays[j] + receive_indexes[j];
+            if (points[l][0] < min) {
+                point = points[l];
+                min = point[0];
+                k = j;
+            }
+        }
+        out[i] = point;
+        receive_indexes[k]++;
+    }
+}
 
 /*
 get_center implementation that uses parallel sorting by regular sampling
@@ -265,25 +327,36 @@ Assumes that n_points_global >= n_procs^2.
 void mpi_psrs_get_center(double* out) {
     double pivots[n_procs - 1];
 
-    psrs_sort_local_projections();
-    mpi_psrs_get_pivots(pivots);
-    
+    int receive_counts[n_procs];
+    int receive_displays[n_procs];
+
     int send_counts[n_procs];
     int send_displays[n_procs];
 
-    psrs_count_values_to_send(send_counts, send_displays, pivots);
+    long receive_processes_n_points[n_procs];
 
-    int receive_counts[n_procs];
-    int receive_displays[n_procs];
-    long n_points_receive = mpi_psrs_count_values_to_receive(send_counts,receive_counts,receive_displays);
+    MPI_Request request;
 
-    double** receive_buffer = mpi_psrs_points_exchange(send_counts,send_displays,receive_counts,receive_displays,n_points_receive);
-    
-    qsort(receive_buffer, n_points_receive, sizeof(double*), compare_point);
+    psrs_sort_local_projections();
+    mpi_psrs_get_pivots(pivots);
 
-    long receive_array[n_procs];
-    mpi_psrs_gather_npoints_receive(receive_array ,n_points_receive);
+    psrs_get_send_info(pivots, send_counts, send_displays);
 
-    mpi_psrs_copy_median_projection(receive_buffer, receive_array, out);
+    long n_points_receive = psrs_get_receive_info(send_counts, receive_counts, receive_displays);
 
+    double **receive_buffer = mpi_psrs_exchange_projections(send_counts, send_displays, receive_counts, receive_displays, n_points_receive);
+
+    mpi_async_psrs_gather_n_points_receive(receive_processes_n_points, n_points_receive, &request);
+
+    /* sort received projections while gathering receive_processes_n_points asynchronously */
+    double **sorted_projections = (double**) malloc(sizeof(double*) * n_points_receive);
+    psrs_merge_sorted_point_list_partitions(receive_buffer, receive_counts, receive_displays, n_points_receive, sorted_projections);
+
+    MPI_Wait(&request, MPI_STATUS_IGNORE);
+
+    mpi_psrs_copy_median_projection(sorted_projections, receive_processes_n_points, out);
+
+    free(*receive_buffer);
+    free(receive_buffer);
+    free(sorted_projections);
 }
